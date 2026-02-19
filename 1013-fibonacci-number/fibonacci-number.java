acaa7ced109dcf55e3c69211ceeedb6f266814f5
Harshit <152162533+HarshitGupta04974@users.noreply.github.com>
2026-02-19 19:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (85.83%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -6,22 +6,27 @@ class Solution {
     //     if(dp[n]!=-1) return dp[n];
     //     return dp[n]=fib_dp(n-1,dp)+fib_dp(n-2,dp);
     // }
-    private static int fib_tab(int n)
-    {   if(n<=1) return n;
-        int dp[]=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-    }
+    // private static int fib_tab(int n)
+    // {   if(n<=1) return n;
+    //     int dp[]=new int[n+1];
+    //     dp[0]=0;
+    //     dp[1]=1;
+    //     for(int i=2;i<=n;i++)
+    //     {
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
     public int fib(int n) {
-       
-        // Arrays.fill(dp,-1);
-        // return fib_dp(n,dp);
-
-        return fib_tab(n);
+        if(n<=1) return n;
+       int prev2=0;
+       int prev1=1;
+       for(int i=2;i<=n;i++)
+       {
+        int cur=prev2+prev1;
+        prev2=prev1;
+        prev1=cur;
+       }
+       return prev1;
     }
 }
