c0fdd45aad1e886784e3c08817aadee916ff20d9
Harshit <152162533+HarshitGupta04974@users.noreply.github.com>
2026-02-25 19:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (44.66%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,28 +1,47 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m=obstacleGrid.length;
-        int n=obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return dfs(0,0,obstacleGrid, dp);
-
-    }
-private static int dfs(int i, int j, int[][] obstacleGrid, int[][] dp){
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        if(i>=m || j>=n ||obstacleGrid[i][j]==1){
-            return 0;
-        }
-        if(i==m-1&&j==n-1){
+    // private static int help(int[][] arr,int m,int n)
+    // {
+    //     if(m==0&&n==0)
+    //     {
+    //         return 1;
+    //     }
+    //     if(m<0||n<0) return 0;
+    //     if(arr[m][n]==1) return 0;
+    //     int up=0, left=0;
+    //     up=help(arr,m-1,n);
+    //     left=help(arr,m,n-1);
+    //     return up+left;
+    // }
+        private static int dpS(int[][] arr,int m,int n,int dp[][])
+    {
+        if(m<0||n<0) return 0;
+        if(arr[m][n]==1) return 0;
+        if(m==0&&n==0)
+        {
             return 1;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+        if(dp[m][n]!=-1) return dp[m][n];
+        int up=0, left=0;
+        up=dpS(arr,m-1,n,dp);
+        left=dpS(arr,m,n-1,dp);
+        return dp[m][n]=up+left;
+    }
+    public int uniquePathsWithObstacles(int[][] arr) {
+        int m=arr.length;
+        int n=arr[0].length;
+        if(m==1&&n==1&&arr[m-1][n-1]==1) return 0;
+        if(m==1&&n==1&&arr[m-1][n-1]==0) return 1;
+        // return help(obstacleGrid,m-1,n-1);
+        int dp[][]=new int[m+1][n+1];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                dp[i][j]=-1;
+            }
         }
-        dp[i][j]=dfs(i+1, j, obstacleGrid, dp) + dfs(i, j+1, obstacleGrid, dp);
-        return dp[i][j];
+        return dpS(arr,m-1,n-1,dp);
 
-}
+    }
+
 }
