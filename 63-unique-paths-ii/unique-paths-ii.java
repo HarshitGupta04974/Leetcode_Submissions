f7c93094bfbdb3c4c147531b8fae26832978235f
Harshit <152162533+HarshitGupta04974@users.noreply.github.com>
2026-03-02 13:46:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (82.02%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -12,19 +12,46 @@ class Solution {
     //     left=help(arr,m,n-1);
     //     return up+left;
     // }
-        private static int dpS(int[][] arr,int m,int n,int dp[][])
+    //     private static int dpS(int[][] arr,int m,int n,int dp[][])
+    // {
+    //     if(m<0||n<0) return 0;
+    //     if(arr[m][n]==1) return 0;
+    //     if(m==0&&n==0)
+    //     {
+    //         return 1;
+    //     }
+    //     if(dp[m][n]!=-1) return dp[m][n];
+    //     int up=0, left=0;
+    //     up=dpS(arr,m-1,n,dp);
+    //     left=dpS(arr,m,n-1,dp);
+    //     return dp[m][n]=up+left;
+    // }
+    private static int tab(int [][] arr)
     {
-        if(m<0||n<0) return 0;
-        if(arr[m][n]==1) return 0;
-        if(m==0&&n==0)
+        int m=arr.length;
+        int n=arr[0].length;
+        int dp[][]=new int[m+1][n+1];
+        for(int i=0;i<m;i++)
         {
-            return 1;
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i][j]==1)dp[i][j]=0;
+                else if(i==0&&j==0) dp[i][j]=1;
+                else{
+                    int up=0,down=0;
+                    if(i>0)
+                    {
+                        up=dp[i-1][j];
+                    }
+                    if(j>0)
+                    {
+                        down=dp[i][j-1];
+                    }
+                dp[i][j]=up+down;
+                }
+            }
         }
-        if(dp[m][n]!=-1) return dp[m][n];
-        int up=0, left=0;
-        up=dpS(arr,m-1,n,dp);
-        left=dpS(arr,m,n-1,dp);
-        return dp[m][n]=up+left;
+        return dp[m-1][n-1];
     }
     public int uniquePathsWithObstacles(int[][] arr) {
         int m=arr.length;
@@ -32,16 +59,16 @@ class Solution {
         if(m==1&&n==1&&arr[m-1][n-1]==1) return 0;
         if(m==1&&n==1&&arr[m-1][n-1]==0) return 1;
         // return help(obstacleGrid,m-1,n-1);
-        int dp[][]=new int[m+1][n+1];
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                dp[i][j]=-1;
-            }
-        }
-        return dpS(arr,m-1,n-1,dp);
-
+        // int dp[][]=new int[m+1][n+1];
+        // for(int i=0;i<m;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return dpS(arr,m-1,n-1,dp);
+        return tab(arr);
     }
 
 }
