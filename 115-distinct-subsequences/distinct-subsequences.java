13645a360b8f99bc426662bff09801a282c1e3d0
Harshit <152162533+HarshitGupta04974@users.noreply.github.com>
2026-04-09 23:57:51 +0530

    Time: 20 ms (53.73%) | Memory: 54.5 MB (44.46%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,44 @@
+class Solution {
+    private static int func(int i,int j,String s,String t,int dp[][])
+    {
+        if(j<0) return 1;
+        if(i<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s.charAt(i)==t.charAt(j))
+        {
+            return dp[i][j]=func(i-1,j-1,s,t,dp)+func(i-1,j,s,t,dp);
+        }
+        return dp[i][j]=func(i-1,j,s,t,dp);
+    }
+    private static int tab(String s, String t)
+    {
+        int n=s.length();
+        int m=t.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++)dp[i][0]=1;
+        
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s.charAt(i-1)==t.charAt(j-1))
+                {
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+               else{
+                    dp[i][j]=dp[i-1][j];
+               } 
+            }
+        }
+        return dp[n][m];
+    }
+    public int numDistinct(String s, String t) {
+        // int n=s.length();
+        // int m=t.length();
+        // int dp[][]=new int[n][m];
+        // for(int rows[]:dp) Arrays.fill(rows,-1);
+        // return func(n-1,m-1,s,t,dp);
+        return tab(s,t);
+    }
+}
