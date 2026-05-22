6529654bcf577b0b0d6ff5d261db07ca03da5cca
Harshit <152162533+HarshitGupta04974@users.noreply.github.com>
2026-05-23 00:02:40 +0530

    Time: 83 ms (21.71%) | Memory: 237.1 MB (11.77%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -14,11 +14,31 @@ class Solution {
         }
         return dp[ind][prev_ind+1]=len;
     }
-    public int lengthOfLIS(int[] nums) {
+    private static int tab(int nums[])
+    {
         int n=nums.length;
-        int dp[][]=new int[n][n+1];
-        for(int rows[]:dp) Arrays.fill(rows,-1);
-        return func(0,-1,nums,dp); 
+        int dp[][]=new int[n+1][n+1];
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--)
+            {
+                int len=0;
+                len=0+dp[ind+1][prev_ind+1];
+                if(prev_ind==-1||nums[ind]>nums[prev_ind])
+                {
+                    len=Math.max(len,1+dp[ind+1][ind+1]);
+                }
+                dp[ind][prev_ind+1]=len;
+            }
+        }
+        return dp[0][0];
+    }
+    public int lengthOfLIS(int[] nums) {
+        // int n=nums.length;
+        // int dp[][]=new int[n][n+1];
+        // for(int rows[]:dp) Arrays.fill(rows,-1);
+        // return func(0,-1,nums,dp); 
+        return tab(nums);
                
     }
 }
