be2b1e6572e6c46431d84f317fa0074bda28af71
Harshit <152162533+HarshitGupta04974@users.noreply.github.com>
2026-07-17 12:18:06 +0530

    Time: 1 ms (87.24%) | Memory: 45.5 MB (63.62%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,28 @@
 class Solution {
-    private static void help(int nums[],List<List<Integer>> ans,List<Integer> ds,boolean used[]){
-        if(ds.size()==nums.length){
-            ans.add(new ArrayList<>(ds));
-            return;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(used[i]||i>0&&nums[i]==nums[i-1]&&!used[i-1]){
-                    continue;
-            }
-            used[i]=true;
-            ds.add(nums[i]);
-            help(nums,ans,ds,used);
-            used[i]=false;
-            ds.remove(ds.size()-1);
-        }
+   private static void func(int nums[],List<List<Integer>> ans,List<Integer> ds,boolean used[])
+   {
+    if(ds.size()==nums.length)
+    {
+        ans.add(new ArrayList<>(ds));
+        return;
     }
+    for(int i=0;i<nums.length;i++)
+    {
+        if(used[i]||i>0&&nums[i]==nums[i-1]&&!used[i-1])
+        {
+            continue;
+        }
+        used[i]=true;
+        ds.add(nums[i]);
+        func(nums,ans,ds,used);
+        used[i]=false;
+        ds.remove(ds.size()-1);
+    }
+   }
     public List<List<Integer>> permute(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>>ans=new ArrayList<>();
-        help(nums,ans,new ArrayList<>(),new boolean[nums.length]);
+        List<List<Integer>> ans=new ArrayList<>();
+        boolean used[]=new boolean[nums.length];
+        func(nums,ans,new ArrayList<>(),used);
         return ans;
     }
 }
