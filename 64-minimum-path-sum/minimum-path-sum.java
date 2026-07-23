3abe4903967af73604b99856a974461cb182eec4
Harshit <152162533+HarshitGupta04974@users.noreply.github.com>
2026-07-23 11:39:49 +0530

    Time: 1 ms (99.93%) | Memory: 50.3 MB (56.39%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,82 +1,26 @@
 class Solution {
-    private static int help(int[][] grid,int i,int j,int dp[][])
+    private static int func(int i,int j,int grid[][],int dp[][])
     {
-        if(i<0||j<0) return Integer.MAX_VALUE;
-        if(i==0&&j==0) return grid[i][j];
+        if(i<0||j<0)  return (int)1e9;
+        if(i==0&&j==0)
+        {
+            return grid[0][0];
+        }
         if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=grid[i][j]+Math.min(help(grid,i-1,j,dp),help(grid,i,j-1,dp));
-        
+        int up,right;
+        up=grid[i][j]+func(i-1,j,grid,dp);
+        right=grid[i][j]+func(i,j-1,grid,dp);
+        return dp[i][j]=Math.min(up,right);
     }
-    private static int tab(int [][]grid)
-    {
-        int n=grid.length;
-        int m=grid[0].length;
-        int dp[][]=new int[n+1][m+1];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(i==0&&j==0){
-                    dp[i][j]=grid[i][j];
-                }
-                else{
-                int up=Integer.MAX_VALUE,left=Integer.MAX_VALUE;
-                if(i>0)
-                {
-                    up=dp[i-1][j];
-                }
-                if(j>0)
-                {
-                    left=dp[i][j-1];
-                }
-                dp[i][j]=grid[i][j]+Math.min(up,left);
-                }
-            }
-        }
-        return dp[n-1][m-1];
-
-    }
-    private static int space(int [][]grid)
-    {       
-        int n=grid.length;
-        int m=grid[0].length;
-        int prev[]=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            int cur[]=new int[m];
-            for(int j=0;j<m;j++)
-            {
-                if(i==0&&j==0) cur[j]=grid[i][j];
-                else{
-                int up=grid[i][j];
-                if(i>0) up+=prev[j];
-                else up=Integer.MAX_VALUE;
-
-                int left=grid[i][j];
-                if(j>0) left+=cur[j-1];
-                else left=Integer.MAX_VALUE;
-
-                cur[j]=Math.min(left,up);
-                }
-            }
-            prev=cur;
-        }
-        return prev[m-1];
-    }
+    
     public int minPathSum(int[][] grid) {
-        // int n=grid.length;
-        // int m=grid[0].length;
-        // int dp[][]=new int[n+1][m+1];
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<m;j++)
-        //     {
-        //         dp[i][j]=-1;
-        //     }
-        // }
-        // return help(grid,n-1,m-1,dp);
-        // return tab(grid);
-        return space(grid);
-        
+        int n=grid.length;
+        int m=grid[0].length;
+        int dp[][]=new int[n][m];
+        for(int rows[]:dp)
+        {
+            Arrays.fill(rows,-1);
+        }
+        return func(n-1,m-1,grid,dp);
     }
 }
