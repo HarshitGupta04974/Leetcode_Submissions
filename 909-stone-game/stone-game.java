952548386d900a5d7c5e70d31975ce9c38c83bf2
Harshit <152162533+HarshitGupta04974@users.noreply.github.com>
2026-08-02 14:12:16 +0530

    Time: 8 ms (18.78%) | Memory: 46.9 MB (10.85%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,22 @@
+class Solution {
+    private static int func(int i,int j,int nums[],int dp[][])
+    {
+        if(i==j)
+        {
+            return nums[i];
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        int takeLeft=nums[i]-func(i+1,j,nums,dp);
+        int takeRight=nums[j]-func(i,j-1,nums,dp);
+        return dp[i][j]=Math.max(takeLeft,takeRight);
+    }
+    public boolean stoneGame(int[] piles) {
+        int n =piles.length;
+        int dp[][]=new int[n][n];
+        for(int rows[]:dp)
+        {
+            Arrays.fill(rows,-1);
+        }
+        return func(0,n-1,piles,dp)>0;
+    }
+}
