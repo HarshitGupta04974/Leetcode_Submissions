9f34950977340d3c4692ef2636ed829b1cbec222
Harshit <152162533+HarshitGupta04974@users.noreply.github.com>
2026-08-02 14:22:48 +0530

    Time: 5 ms (23.31%) | Memory: 46.8 MB (16.42%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -10,13 +10,38 @@ class Solution {
         int takeRight=nums[j]-func(i,j-1,nums,dp);
         return dp[i][j]=Math.max(takeLeft,takeRight);
     }
-    public boolean stoneGame(int[] piles) {
+    private static int tab(int[]piles)
+    {  
         int n =piles.length;
-        int dp[][]=new int[n][n];
-        for(int rows[]:dp)
+        int dp[][]=new int[n+1][n+1];
+        for(int i=0;i<n;i++)
         {
-            Arrays.fill(rows,-1);
+            for(int j=0;j<n;j++)
+            {
+                if(i==j)
+                dp[i][j]=piles[i];
+            }
         }
-        return func(0,n-1,piles,dp)>0;
+        for(int i =n-1;i>=0;i--)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int takeLeft=piles[i]-dp[i+1][j];
+                int takeRight=piles[j]-dp[i][j-1];
+                dp[i][j]=Math.max(takeLeft,takeRight);
+            }
+        }
+        return dp[0][n-1];
+
+    }
+    public boolean stoneGame(int[] piles) {
+        // int n =piles.length;
+        // int dp[][]=new int[n][n];
+        // for(int rows[]:dp)
+        // {
+        //     Arrays.fill(rows,-1);
+        // }
+        // return func(0,n-1,piles,dp)>0;
+        return tab(piles)>0;
     }
 }
